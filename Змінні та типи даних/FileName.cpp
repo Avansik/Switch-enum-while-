1a858d7d4da5d586082f1d616427e52cd74501b8
artuk <artuk@LENOVO-AV08>
2025-03-04 16:25:19 +0200

    Add project files.

---
+++ Змінні та типи даних/FileName.cpp
@@ -0,0 +1,46 @@
+﻿
+ /* Switch, enum, while
+ #include <iostream>
+    using namespace std;
+	int main() { */
+ // //2.Вивести на екран додатні непарні числа, які не перевищують 100. - while        
+ //  int i = 1; 
+
+ //           while (i <= 100) { 
+ //               cout << i << " ";
+ //               i += 2; 
+ //           }
+ //       
+ 
+ /*  1.Вивести на екран цілі числа від 14 до 123. - while*/
+       /* int i = 14; 
+        while (i <= 123) { 
+            cout << i << " ";
+            i++;
+        }*/
+     /*   5.Вивести на екран цілі непарні  числа : 100 98 96 94...4  2 1. - do..while*/
+        /*    int i = 100; 
+            do {
+                cout << i << " ";
+                i -= 2; 
+            } while (i >= 1); 
+            return 0;*/
+         
+        //3.Дано N(N > 0).Вводяться N чисел.Знайти кількість від’ємних серед них. - while
+        /*    int countnumber; 
+            cout << "Print how many numbers to enter: ";
+            cin >> countnumber;
+            int i = 0;       
+            int number;    
+            int negativeCount = 0;
+            while (i < countnumber) {
+                cout << "Enter number " << i + 1 << ": ";
+                cin >> number;
+
+                if (number < 0) { 
+                    negativeCount++;
+                }
+                i++;
+            }
+            cout << "Count of negative numbers: " << negativeCount << endl;
+        }*/
